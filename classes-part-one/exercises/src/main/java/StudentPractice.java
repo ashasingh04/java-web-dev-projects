f926a40addd9f18b37affe395873136d6640c2ad
ashasingh04 <asha.sgsits@gmail.com>
2024-04-08 14:26:02 -0500

    Classes Part 1 exercise completed

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,7 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student student = new Student("Asha Singh", 12345, 1, 4.0);
+        System.out.println(student.getName());
     }
 }
